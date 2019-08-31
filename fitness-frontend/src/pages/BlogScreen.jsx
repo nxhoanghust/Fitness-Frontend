6d4be416baee321a36722e97e195d62e13fbd5da
tunggao <31013999+tunggao@users.noreply.github.com>
2019-08-31 13:33:16 +0700

    update homeScreen
    Homescreen

--- fitness-frontend/src/pages/BlogScreen.jsx
+++ fitness-frontend/src/pages/BlogScreen.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-import { Breadcrumb, Icon, Modal, Input, Upload, message, Button } from "antd";
+import {
+  Breadcrumb,
+  Icon,
+  Modal,
+  Input,
+  Upload,
+  message,
+  Tag,
+  Row,
+  Col,
+  Carousel
+} from "antd";
 import "./BlogScreen.css";
 import "antd/dist/antd.css";
+import { TweenOneGroup } from "rc-tween-one";
 
 const { TextArea } = Input;
 
@@ -15,12 +27,125 @@ function getBase64(file) {
 }
 
 class BlogScreen extends React.Component {
+  componentDidMount() {
+    const email = window.localStorage.getItem("email");
+    const fullName = window.localStorage.getItem("fullName");
+    if (!email || !fullName) {
+      fetch("http://localhost:3001/users/test", {
+        credentials: "include",
+        method: "GET"
+      })
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          //console.log(data);
+          if (data.success == true) {
+            this.setState({
+              currentUser: {
+                email: data.data.email,
+                fullName: data.data.fullName
+              }
+            });
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          window.alert(error.message);
+        });
+    } else {
+      this.setState({
+        currentUser: {
+          email: email,
+          fullName: fullName
+        }
+      });
+    }
+    //take the post
+    fetch(`http://localhost:3001/posts/get`, { method: "GET" })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        //console.log(data);
+        this.setState({
+          data: data.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        message.error(error.message);
+      });
+    // end test
+  }
+
   state = {
+    pageNumber: 1,
+    PageSize: 5,
+    currentUser: {
+      email: "",
+      fullName: ""
+    },
     content: "",
     previewVisible: false,
     previewImage: "",
-    fileList: []
+    fileList: [],
+    tags: [],
+    inputValue: "",
+    title: "",
+    inputVisible: false,
+    data: []
   };
+  //tags
+  handleTagClose = removedTag => {
+    const tags = this.state.tags.filter(tag => tag !== removedTag);
+    console.log(tags);
+    this.setState({ tags });
+  };
+
+  showTagInput = () => {
+    this.setState({ inputVisible: true }, () => this.input.focus());
+  };
+
+  handleInputTageChange = e => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleInputTagConfirm = () => {
+    const { inputValue } = this.state;
+    let { tags } = this.state;
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      tags = [...tags, inputValue];
+    }
+    console.log(tags);
+    this.setState({
+      tags,
+      inputVisible: false,
+      inputValue: ""
+    });
+  };
+
+  saveInputRef = input => (this.input = input);
+
+  forMap = tag => {
+    const tagElem = (
+      <Tag
+        closable
+        onClose={e => {
+          e.preventDefault();
+          this.handleTagClose(tag);
+        }}
+      >
+        {tag}
+      </Tag>
+    );
+    return (
+      <span key={tag} style={{ display: "inline-block" }}>
+        {tagElem}
+      </span>
+    );
+  };
+
   handlePictureCancel = () => this.setState({ previewVisible: false });
   handleChange = ({ fileList }) => {
     if (fileList[fileList.length - 1].size > 5000000) {
@@ -54,13 +179,66 @@ class BlogScreen extends React.Component {
   };
   //OnSubmit
   handleOk = e => {
-    console.log(this.state);
+    console.log(this.state.fileList);
     if (!this.state.content) {
-      message.error("Please input your content ");
-    } else if (!this.state.fileList) {
-      message.error("Please input your image ");
-    } else {
+      message.error("Please Input your content ");
+    } else if (!this.state.title) {
+      message.error("Please Input your Title ");
+    } else if (this.state.fileList.length === 0) {
+      message.error("Please Input your image ");
+    } else if (this.state.tags.length === 0) {
+      message.error("Please Enter a tag! Tags will have people easy to search");
       //upload img
+    } else {
+      const formData = new FormData();
+      for (const item of this.state.fileList) {
+        formData.append("images", item.originFileObj);
+        console.log(item);
+      }
+      fetch("http://localhost:3001/upload/image", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          Accept: "application/json"
+          //"Content-Type": "multipart/form-data"
+        },
+        body: formData
+      })
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          // console.log(data.imageUrl);
+          console.log(this.state);
+          fetch("http://localhost:3001/posts/create", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              content: this.state.content,
+              imageUrl: data.imageUrl,
+              tag: this.state.tags,
+              title: this.state.title
+            })
+          })
+            .then(res => {
+              return res.json();
+            })
+            .then(data1 => {
+              console.log(data1);
+            })
+            .catch(err => {
+              console.log(err);
+              this.setState({
+                errMessage: err.message
+              });
+            });
+        })
+        .catch(error => {
+          message.error(error);
+        });
     }
   };
   //OnCancel
@@ -74,9 +252,20 @@ class BlogScreen extends React.Component {
       content: e.target.value
     });
   };
+  handleTitleChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  //pagination
+
   render() {
-    console.log(this.state.content);
+    console.log(this.state);
+
     const { previewVisible, previewImage, fileList } = this.state;
+    const { tags, inputVisible, inputValue } = this.state;
+    const tagChild = tags.map(this.forMap);
     const uploadButton = (
       <div>
         <Icon type="plus" />
@@ -84,30 +273,42 @@ class BlogScreen extends React.Component {
       </div>
     );
     return (
-      <div className="ml-5 mr-5">
+      <div className="ml-5 mr-5 pt-3">
         <Breadcrumb separator=">">
           <Breadcrumb.Item>
             <Breadcrumb.Item className="breadcrumb-item">
-              <a href="/">
+              <a href="/" style={{ fontSize: "17px" }}>
                 <Icon type="home" />
                 Home/
               </a>
             </Breadcrumb.Item>
             <Breadcrumb.Separator>/</Breadcrumb.Separator>
             <Breadcrumb.Item className="breadcrumb-item">
-              <a href="/Blogs">Blogs/</a>
+              <a href="/Blogs" style={{ fontSize: "17px" }}>
+                Blogs/
+              </a>
             </Breadcrumb.Item>
           </Breadcrumb.Item>
         </Breadcrumb>
-        <div>
-          <button
-            type="button"
-            className="btn btn-outline-dark add-post mr-5"
-            onClick={this.showModal}
-          >
-            <Icon type="plus" style={{ fontSize: 14 }} />
-            New Post
-          </button>
+
+        <div className="row">
+          {this.state.currentUser.email ? (
+            <Row>
+              <button
+                type="button"
+                className="btn btn-outline-dark add-post "
+                onClick={this.showModal}
+                style={{
+                  width: "200px",
+                  textAlign: "center",
+                  fontWeight: "bold"
+                }}
+              >
+                <Icon type="plus" style={{ fontSize: 14 }} />
+                New Post
+              </button>
+            </Row>
+          ) : null}
           <Modal
             title="Basic Modal"
             visible={this.state.visible}
@@ -115,6 +316,12 @@ class BlogScreen extends React.Component {
             onCancel={this.handleCancel}
           >
             <form>
+              <h3 className="modal-upload">Title:</h3>
+              <Input
+                placeholder="Title"
+                value={this.state.title}
+                onChange={this.handleTitleChange}
+              ></Input>
               <h3 className="modal-upload">Content:</h3>
               <TextArea
                 id="content"
@@ -141,8 +348,85 @@ class BlogScreen extends React.Component {
             >
               <img alt="example" style={{ width: "100%" }} src={previewImage} />
             </Modal>
+            <div>
+              <div style={{ marginBottom: 16 }}>
+                <TweenOneGroup
+                  enter={{
+                    scale: 0.8,
+                    opacity: 0,
+                    type: "from",
+                    duration: 100,
+                    onComplete: e => {
+                      e.target.style = "";
+                    }
+                  }}
+                  leave={{ opacity: 0, width: 0, scale: 0, duration: 200 }}
+                  appear={false}
+                >
+                  {tagChild}
+                </TweenOneGroup>
+              </div>
+              {inputVisible && (
+                <Input
+                  ref={this.saveInputRef}
+                  type="text"
+                  size="small"
+                  style={{ width: 78 }}
+                  value={inputValue}
+                  onChange={this.handleInputTageChange}
+                  onBlur={this.handleInputTagConfirm}
+                  onPressEnter={this.handleInputTagConfirm}
+                />
+              )}
+              {!inputVisible && (
+                <Tag
+                  onClick={this.showTagInput}
+                  style={{ background: "#fff", borderStyle: "dashed" }}
+                >
+                  <Icon type="plus" /> New Tag
+                </Tag>
+              )}
+            </div>
           </Modal>
         </div>
+        <Row>
+          <Col span={19} className="post">
+            {this.state.data.map(i => {
+              return (
+                <Carousel autoplay>
+                  {i.srcUrl.map(item => {
+                    //console.log(item);
+                    return (
+                      <div>
+                        <img src={item} style={{ width: "300px" }}></img>
+                      </div>
+                    );
+                  })}
+                  <div>{i.content} </div>
+                </Carousel>
+              );
+            })}
+          </Col>
+          <Col span={4}>
+            <div class="btn-group-vertical" style={{ width: "100%" }}>
+              <button type="button" class="btn btn-secondary button-group">
+                Categories
+              </button>
+              <button type="button" class="btn btn-secondary button-group">
+                Boxing
+              </button>
+              <button type="button" class="btn btn-secondary button-group">
+                Yoga
+              </button>
+              <button type="button" class="btn btn-secondary button-group">
+                Diet
+              </button>
+              <button type="button" class="btn btn-secondary button-group">
+                Street Workout
+              </button>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
--- fitness-frontend/src/pages/HomePageScreen.jsx
+++ fitness-frontend/src/pages/HomePageScreen.jsx
@@ -1,7 +1,112 @@
-import React from "react";
+import React, { Component } from "react";
+import './dashboard.css';
+import './demo.css'
+import './footer-distributed-with-address-and-phones.css'
+import './homescreen.css'
 class HomePageScreen extends React.Component {
   render() {
-    return <div>HomePageScreen</div>;
+    return <div>
+      <div className="DashBoard">
+        <br></br>
+        <br></br>
+        <header>
+          <div className="quote text-center mx-auto mt-2">
+            <h4>Premium</h4>
+            <p>Now, there are obviously tons of core exercises you can do,
+              and you probably know a handful already.
+              But if you're looking for some new ideas, we've got you covered.
+              Sure, traditional core exercises, like mountain climbers, leg lifts, and planks, are great.
+              They get the job done. I, however, tend to get a little bored of doing the same things over and over again.
+               Of course, consistency is a good thing, and also a great way to track your progress—but let's be honest, sometimes we all just need a new challenge to keep our fitness routines feeling fresh and exciting.</p>
+          </div>
+        </header>
+      </div>
+      <div className="HomeScreen">
+        <div className="body container-fluid">
+          <div className="row intro-container" ref='foo1'>
+            <div className="col-6 image-intro">
+              <img src='..img/menu-card.png' className="img-fluid" alt="" />
+            </div>
+            <div className="col-6">
+              <div className="content-intro">
+                <h4>Make your own healthy menu</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, est a
+                    interdum congue, lectus ligula varius est, et hendrerit sapien diam nec lacus.
+                        Phasellus ullamcorper mauris id magna porta finibus. Nulla suscipit erat a ex posuere egestas.</p>
+              </div>
+
+            </div>
+          </div>
+          <div className="row intro-container-2" ref='foo2'>
+            <div className="col-6">
+              <div className="content-intro-2">
+                <h4>Make your own healthy menu</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, est a
+                    interdum congue, lectus ligula varius est, et hendrerit sapien diam nec lacus.
+                        Phasellus ullamcorper mauris id magna porta finibus. Nulla suscipit erat a ex posuere egestas.</p>
+              </div>
+            </div>
+            <div className="col-6 image-intro">
+              <img src='..img/menu-card.png' className="img-fluid" alt="" />
+            </div>
+          </div>
+
+          <div className="row intro-container" ref='foo3'>
+            <div className="col-6 image-intro">
+              <img src='..img/menu-card.png' className="img-fluid" alt="" />
+            </div>
+            <div className="col-6">
+              <div className="content-intro">
+                <h4>Make your own healthy menu</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, est a
+                    interdum congue, lectus ligula varius est, et hendrerit sapien diam nec lacus.
+                        Phasellus ullamcorper mauris id magna porta finibus. Nulla suscipit erat a ex posuere egestas.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+      </div>
+      <div className="footer">
+        <footer className="footer-distributed">
+          <div className="footer-left">
+            <h3>Company<span>logo</span></h3>
+            <p className="footer-links">
+            </p>
+            <p className="footer-company-name">Company Name &copy; 2015</p>
+          </div>
+          <div className="footer-center">
+            <div>
+              <i className="fa fa-map-marker"></i>
+              <p><span>21 Revolution Street</span> Paris, France</p>
+            </div>
+            <div>
+              <i className="fa fa-phone"></i>
+              <p>+1 555 123456</p>
+            </div>
+            <div>
+              <i className="fa fa-envelope"></i>
+              <p><a href="mailto:support@company.com">support@company.com</a></p>
+            </div>
+          </div>
+          <div className="footer-right">
+            <p className="footer-company-about">
+              <span>About the company</span>
+              Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor
+              lacus vehicula sit amet.
+                </p>
+            <div className="footer-icons">
+              <a href="#"><i className="fa fa-facebook"><icon type="facebook"/></i></a>
+              <a href="#"><i className="fa fa-twitter"></i></a>
+              <a href="#"><i className="fa fa-linkedin"></i></a>
+              <a href="#"><i className="fa fa-github"></i></a>
+
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>;
   }
 }
 
